2b2d5199c037c79dded31bbfcd6fd3857b079d98
Jasper Robeer <jasper.robeer@gmail.com>
2017-03-30 16:29:13 +0200

    Add 3D vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod vector3;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/vector3.rs
@@ -0,0 +1,119 @@
+//! Representation of a 3D vector.
+
+use std::ops::{Add, Sub, Mul, Div, Neg};
+
+#[derive(Debug)]
+struct Vector3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vector3 {
+    fn new(x: f32, y: f32, z: f32) -> Vector3 {
+        Vector3 { x: x, y: y, z: z }
+    }
+}
+
+/// Computes the dot product of two vectors.
+fn dot(a: Vector3, b: Vector3) -> f32 {
+    a.x * b.x + a.y * b.y + a.z * b.z
+}
+
+/// Computes the cross product of two vectors.
+fn cross(a: Vector3, b: Vector3) -> Vector3 {
+    Vector3 {
+        x: a.y * b.z - a.z * b.y,
+        y: a.z * b.x - a.x * b.z,
+        z: a.x * b.y - a.y * b.x
+    }
+}
+
+/// The following operators are defined below:
+///     Vector3 + Vector3
+///     Vector3 - Vector3
+///     Vector3 * Vector3
+///     Vector3 / Vector3
+///     Vector3 * f32
+///     Vector3 / f32
+///     -Vector3
+
+impl Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z
+        }
+    }
+}
+
+impl Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z
+        }
+    }
+}
+
+impl Mul for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z
+        }
+    }
+}
+
+impl Div for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, rhs: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z
+        }
+    }
+}
+
+impl Mul<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: f32) -> Vector3 {
+        Vector3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs
+        }
+    }
+}
+
+impl Div<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, rhs: f32) -> Vector3 {
+        Vector3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs
+        }
+    }
+}
+
+impl Neg for Vector3 {
+    type Output = Vector3;
+
+    fn neg(self) -> Vector3 {
+        Vector3 { x: -self.x, y: -self.y, z: -self.z }
+    }
+}
